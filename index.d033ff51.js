05531729a51278e385871bbf0b1b8b73772816e7
blessfire <64978979+blessfire@users.noreply.github.com>
2023-07-15 08:55:23 +0000

    Deploying to gh-pages from @ Arch819/goit-js-blended4-5@6f2c15e72ac79871713a5fb67534cb7144f05163 🚀

---
+++ index.d033ff51.js
@@ -0,0 +1,2 @@
+document.querySelector(".login-form").addEventListener("input",(function(t){const{name:n,value:o}=t.target;e[n]=o}));let e={};
+//# sourceMappingURL=index.d033ff51.js.map
---
+++ index.fcc25826.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".login-form").addEventListener("input",(function(n){var t=n.target,r=t.name,a=t.value;e[r]=a}));var e={}}();
+//# sourceMappingURL=index.fcc25826.js.map
