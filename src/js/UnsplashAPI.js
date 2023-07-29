401c23e02f338404d4df7ce40f7a3163411e68a7
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2023-07-29 11:54:25 +0200

    12345

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -3,13 +3,23 @@
 export class UnsplashAPI {
   #BASE_URL = 'https://api.unsplash.com/search/photos';
   #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
-  #query = "";
+  #query = '';
+  #searchParams = new URLSearchParams({
+    per_page: 12,
+    orientation: 'portrait',
+    client_id: this.#API_KEY,
+  });
+
   getPopularImage(page) {
-    const url = `${
-      this.#BASE_URL
-    }?page=${page}&query=popular&per_page=12&orientation=portrait&client_id=${
-      this.#API_KEY
+    // const url = `${
+    //   this.#BASE_URL
+    // }?page=${page}&query=popular&per_page=12&orientation=portrait&client_id=${
+    //   this.#API_KEY
+    // }`;
+    const url = `${this.#BASE_URL}?page=${page}&query=popular&${
+      this.#searchParams
     }`;
+
     return fetch(url).then(response => {
       if (!response.ok) {
         throw new Error(response.status);
@@ -18,11 +28,13 @@ export class UnsplashAPI {
     });
   }
   getImagesByQuery(page) {
-    const url = `${
-      this.#BASE_URL
-    }?page=${page}&query=${this.#query}&per_page=12&orientation=portrait&client_id=${
-      this.#API_KEY
+    // const url = `${this.#BASE_URL}?page=${page}&query=${
+    //   this.#query
+    // }&per_page=12&orientation=portrait&client_id=${this.#API_KEY}`;
+    const url = `${this.#BASE_URL}?page=${page}&query=${this.#query}&${
+      this.#searchParams
     }`;
+
     return fetch(url).then(response => {
       if (!response.ok) {
         throw new Error(response.status);
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -8,11 +8,29 @@ const apiGallery = new UnsplashAPI();
 
 const container = document.getElementById('tui-pagination-container');
 const form = document.querySelector('.js-search-form');
+
 const options = {
   totalItems: 0,
   itemsPerPage: 12,
   visiblePages: 5,
   page: 1,
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
 };
 const pagination = new Pagination(container, options);
 
@@ -51,13 +69,13 @@ function onSubmitForm(event) {
 }
 
 function getPopular(event) {
-    const currentPage = event.page;
-    apiGallery.getPopularImage(currentPage).then(({ results, total }) => {
-      const markup = createGalleryCard(results);
-      galleryRef.innerHTML = markup;
-    });
-};
-  
+  const currentPage = event.page;
+  apiGallery.getPopularImage(currentPage).then(({ results, total }) => {
+    const markup = createGalleryCard(results);
+    galleryRef.innerHTML = markup;
+  });
+}
+
 function getByQuery(event) {
   const currentPage = event.page;
   apiGallery.getImagesByQuery(currentPage).then(({ results, total }) => {
