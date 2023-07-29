f2a55b9b0b196258f99ee7531a8b32eb0624e1c4
Serhiy8 <markovsm86@gmail.com>
2023-07-29 12:03:43 +0300

    task-02

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,5 +1,25 @@
-import { UnsplashAPI } from "./UnsplashAPI";
+import { UnsplashAPI } from './UnsplashAPI';
+import { createGalleryCard } from './createGalleryCard';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
 
-const apiGallery = new UnsplashAPI()
+const apiGallery = new UnsplashAPI();
 
-apiGallery.getPopularImage(1).then(data=> console.log(data));
+const container = document.getElementById('tui-pagination-container');
+const options = {
+  totalItems: 0,
+  itemsPerPage: 12,
+  visiblePages: 5,
+  page: 1,
+};
+const pagination = new Pagination(container, options);
+
+const page = pagination.getCurrentPage();
+
+const galleryRef = document.querySelector('.js-gallery');
+
+apiGallery.getPopularImage(page).then(({ results, total }) => {
+  pagination.reset(total);
+  const markup = createGalleryCard(results);
+  galleryRef.innerHTML = markup;
+});
